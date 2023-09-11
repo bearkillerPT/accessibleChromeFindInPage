e9b54e16276777a9e771648ea494f4e3ef050336
bearkillerPT <gilteixeira@ua.pt>
2023-09-11 02:52:00 +0100

    good ui with settings customization

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
 // main.js
 console.log('Content script loaded');
 
-// Define variables to track settings
-let blinkInterval = 500; // Default blink interval in milliseconds
-let numBlinks = 3; // Default number of blinks
-let numSurroundingWords = 0; // Default number of words to blink around the search term
+// Initialize default settings (if needed)
+const defaultSettings = {
+    blinkInterval: 500, // Default blink interval in milliseconds
+    numBlinks: 3,       // Default number of blinks
+    numSurroundingWords: 1, // Default number of surrounding words
+};
+let blinkInterval = 1000
+let numBlinks = 3
+let numSurroundingWords = 5
+// Initialize settings from storage or use default settings
+chrome.storage.sync.get(defaultSettings, (result) => {
+    // The result object will contain the stored settings, or the default settings if none are stored.
+    const storedSettings = result;
+    if (!storedSettings.settings) {
+        storedSettings.settings = defaultSettings;
+    }
+    blinkInterval = storedSettings.settings.blinkInterval;
+    numBlinks = storedSettings.settings.numBlinks;
+    numSurroundingWords = storedSettings.settings.numSurroundingWords;
+    // Listen for messages from the popup or content scripts
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message.action === 'getSettings') {
+            // Send the stored settings to the requesting script (popup or content script)
+            sendResponse(storedSettings);
+        } else if (message.action === 'setSettings') {
+            // Update and save the settings to storage
+            const updatedSettings = message.settings;
+            chrome.storage.sync.set(updatedSettings, () => {
+                // Send a confirmation message (if needed)
+                blinkInterval = updatedSettings.blinkInterval;
+                numBlinks = updatedSettings.numBlinks;
+                numSurroundingWords = updatedSettings.numSurroundingWords;
+                sendResponse('Settings updated successfully');
+            });
+        }
+    });
+});
+
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message)
@@ -30,12 +64,13 @@ function blinkLinesWithSearchTerm(searchTerm, tabId) {
         function: (searchTerm, blinkInterval, numBlinks, numSurroundingWords) => {
             const removeBlinkingStyles = () => {
                 const blinkingElements = document.querySelectorAll('.blink');
+                // merge the blink and blink-off texts into one text node
                 blinkingElements.forEach((element) => {
-                    const parent = element.parentNode.parentNode;
-                    if (parent === null) return;
-                    // remove the span element and return it to the original parent with just text
-                    parent.innerHTML = parent.textContent;
-                });
+                    const parent = element.parentNode;
+                    if(!parent) return;
+                    parent.innerHTML = parent.innerHTML = parent.textContent
+                }
+                );
             };
 
             const applyBlinkingStyles = () => {
@@ -44,19 +79,18 @@ function blinkLinesWithSearchTerm(searchTerm, tabId) {
                 const blinkIntervalId = setInterval(() => {
                     if (currentBlinks <= 0) {
                         clearInterval(blinkIntervalId);
-                        element.style.backgroundColor = originalBackgroundColor;
                         return;
                     }
                     const blinkingElements = document.querySelectorAll('.blink');
                     const offBlinkingElements = document.querySelectorAll('.blink-off');
-                    if(currentBlinks % 2 === 0) {
+                    if (currentBlinks % 2 === 0) {
                         blinkingElements.forEach((element) => {
                             element.style.backgroundColor = 'transparent';
                         });
                         offBlinkingElements.forEach((element) => {
                             element.style.backgroundColor = 'yellow';
                         });
-                        
+
                     }
                     else {
                         blinkingElements.forEach((element) => {
@@ -75,29 +109,34 @@ function blinkLinesWithSearchTerm(searchTerm, tabId) {
 
             const findAndMatchText = (element, searchText) => {
                 if (element.nodeType === Node.TEXT_NODE) {
-                    let replacedHTML = element.textContent;
-                    // for each searchTerm match
                     let contentStrings = element.textContent.split(" ");
                     const searchTermRegex = new RegExp(searchText, 'gi');
+
                     for (let i = 0; i < contentStrings.length; i++) {
                         if (contentStrings[i].match(searchTermRegex)) {
                             // surround the match with a span element
                             contentStrings[i] = contentStrings[i].replace(searchTermRegex, `<span class="blink">$&</span>`);
-                        }
-                        else {
-                            contentStrings[i] = `<span class="blink-off">${contentStrings[i]}</span>`;
+                            for (let j = 1; j <= numSurroundingWords; j++) {
+                                if (i - j >= 0) {
+                                    contentStrings[i - j] = `<span class="blink-off">${contentStrings[i - j]}</span>`;
+                                }
+                                if (i + j < contentStrings.length) {
+                                    contentStrings[i + j] = `<span class="blink-off">${contentStrings[i + j]}</span>`;
+                                }
+                            }
                         }
                     }
+
                     const div = document.createElement('div');
                     div.innerHTML = contentStrings.join(" ");
                     element.parentNode.replaceChild(div, element);
                     const blinkElements = document.querySelectorAll('.blink');
-                    blinkElements.forEach((element) => {
-                        element.style.backgroundColor = 'yellow';
-                        element.style.color = 'black';
+                    blinkElements.forEach((blinkElement) => {
+                        blinkElement.style.backgroundColor = 'yellow';
+                        blinkElement.style.color = 'black';
                     });
                     applyBlinkingStyles();
-                    
+
                 } else if (element.nodeType === Node.ELEMENT_NODE) {
                     for (let i = 0; i < element.childNodes.length; i++) {
                         findAndMatchText(element.childNodes[i], searchText);
--- popup.js
+++ popup.js
@@ -1,24 +1,66 @@
-console.log('Popup script loaded');
-// popup.js
+document.addEventListener('DOMContentLoaded', function () {
+  const settingsButton = document.getElementById('settingsButton');
+  const settingsPopup = document.getElementById('settingsPopup');
+  const closeButton = document.getElementById('closeButton');
+  const saveButton = document.getElementById('saveButton');
+  const blinkIntervalInput = document.getElementById('blinkInterval');
+  const numBlinksInput = document.getElementById('numBlinks');
+  const numSurroundingWordsInput = document.getElementById('numSurroundingWords');
 
-// Function to toggle the settings panel visibility
-function toggleSettingsPanel() {
-    const settingsPanel = document.getElementById('settingsPanel');
-    settingsPanel.style.display = settingsPanel.style.display === 'none' ? 'block' : 'none';
-  }
-  
-  // Event listener for the settings button
-  document.getElementById('openSettings').addEventListener('click', toggleSettingsPanel);
-  
+  // Load settings from the background script
+  chrome.runtime.sendMessage({ action: 'getSettings' }, (response) => {
+      if (response) {
+          blinkIntervalInput.value = response.blinkInterval || 1000;
+          numBlinksInput.value = response.numBlinks || 3;
+          numSurroundingWordsInput.value = response.numSurroundingWords || 5;
+      }
+  });
+
+  settingsButton.addEventListener('click', () => {
+      settingsPopup.style.display = 'block';
+
+      // Calculate the new height based on the settings popup content
+      const popupHeight = settingsPopup.scrollHeight;
+      document.body.style.height = popupHeight + 'px';
+  });
+
+  closeButton.addEventListener('click', () => {
+      settingsPopup.style.display = 'none';
+
+      // Reset the body height when the popup is closed
+      document.body.style.height = 'auto';
+  });
+
+  saveButton.addEventListener('click', () => {
+      const blinkInterval = blinkIntervalInput.value;
+      const numBlinks = numBlinksInput.value;
+      const numSurroundingWords = numSurroundingWordsInput.value;
+
+      // Send the updated settings to the background script
+      chrome.runtime.sendMessage({
+          action: 'setSettings',
+          settings: {
+              blinkInterval,
+              numBlinks,
+              numSurroundingWords,
+          },
+      });
+
+      // Close the settings popup
+      settingsPopup.style.display = 'none';
+
+      // Reset the body height when the popup is closed
+      document.body.style.height = 'auto';
+  });
+
   // Event listener for find-in-page input changes
   document.getElementById('findInput').addEventListener('input', handleFindInputChange);
+
   // Function to handle changes in the find-in-page input
   function handleFindInputChange(event) {
-    const searchTerm = event.target.value;
-    console.log(`Sending message to background script to find "${searchTerm}"`);
-    // Send a message to the background script to perform find-in-page functionality
-    chrome.runtime.sendMessage({ action: 'findInPage', searchTerm });
+      const searchTerm = event.target.value;
+      console.log(`Sending message to background script to find "${searchTerm}"`);
+      // Send a message to the background script to perform find-in-page functionality
+      chrome.runtime.sendMessage({ action: 'findInPage', searchTerm });
   }
-  
-  // ... (Additional logic for settings and messaging to background script)
-  
+});
